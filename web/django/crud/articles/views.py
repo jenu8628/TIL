52a09c481dae6bcd3d93af88e2ba67481ee613c7
jenu8628 <wkfwktka@gmail.com>
2020-09-22 14:36:00 +0900

    0922

---
+++ web/django/crud/articles/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Article
+from .forms import ArticleForm
+
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles': articles,
+    }
+    return render(request, 'articles/index.html', context)
+
+def create(request):
+    if request.method == 'POST':
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save()
+            return redirect('articles:detail', article.pk)
+    else:
+        form = ArticleForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'articles/form.html', context)
+
+def detail(request, article_pk):
+    article = get_object_or_404(Article, pk=article_pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/detail.html', context)
