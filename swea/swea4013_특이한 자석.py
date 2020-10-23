a558730437d831c84e1b1c785a8ea618f5d39af8
jenu8628 <wkfwktka@gmail.com>
2020-10-23 13:11:30 +0900

    1023

--- swea/swea4013_특이한 자석.py
+++ swea/swea4013_특이한 자석.py
@@ -61,3 +61,35 @@ for tc in range(1, T + 1):
         if arr[j][0] == 1:
             cnt += 2 ** j
     print('#{} {}'.format(tc, cnt))
+
+# 쌤풀이
+def rotate(idx, d):
+    if d == 1: #시계방향으로 돌릴 때
+        magnet[idx].insert(0, magnet[idx].pop())
+    else:
+        magnet[idx].append(magnet[idx].pop(0))
+
+def func(idx, d):
+    check[idx] = 1
+    # 오른쪽 자석을 돌릴 수 있다면 돌려
+    if idx < 4 and magnet[idx][2] != magnet[idx+1][6] and not check[idx+1]:
+        func(idx+1, -d)
+    # 왼쪽 자석을 돌릴 수 있다면 돌려
+    if idx > 1 and magnet[idx][6] != magnet[idx-1][2] and not check[idx-1]:
+        func(idx-1, -d)
+    rotate(idx, d)
+
+T = int(input())
+for tc in range(1, T+1):
+    K = int(input())
+    ans = 0
+    magnet = [[0]] + [list(map(int, input().split())) for _ in range(4)]
+    for i in range(K):
+        idx, d = map(int, input().split())
+        # 자석이 돌았는지를 체크하는 리스트
+        check = [0] * 5
+        func(idx, d)
+    for i in range(4):
+        if magnet[i+1][0]:
+            ans+= 1<<i
+    print('#{} {}'.format(tc, ans))
